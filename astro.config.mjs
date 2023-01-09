568480e2313faec728b408bbc434265f3d16337c
Caden <cadenrothzeid@gmail.com>
2023-01-08 21:12:42 -0700

    new look

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-import vue from "@astrojs/vue";
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://caden32.com',
   integrations: [tailwind(), sitemap()],
   markdown: {
-    // syntaxHighlight: 'prism',
     extendDefaultPlugins: true,
   }
 });
